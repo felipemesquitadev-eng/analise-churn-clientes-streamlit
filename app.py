5ac4eaf5f294e5bc96700065fb0d7e091744d854
Felipe Scarpin Mesquita <felipemesquita.dev@gmail.com>
2026-09-09 16:32:30 -0300

    mudanças na página principal

--- app.py
+++ app.py
@@ -6,22 +6,54 @@ st.set_page_config(
     layout="centered"
 )
 
-# 2. Título Principal
-st.title("💳 Previsão e Análise de Churn de Cartões de Crédito")
+def home_page():
+    st.title("💳 Análise e Previsão de Churn de Cartões de Crédito", anchor=False)
 
-st.write("""
-Bem-vindo ao **Dashboard Executivo de Análise de Clientes**. 
+    st.write("""
+    Bem-vindo ao **Dashboard Executivo de Análise de Clientes**. 
 
-Este site[aplicativo] interativo foi desenvolvido para explorar a base de dados de clientes de cartões de crédito, entender os fatores que levam ao cancelamento do serviço (*churn*) e simular o risco de evasão de novos perfis.
-""")
+    Este aplicativo interativo foi desenvolvido para explorar a base de dados de clientes de cartões de crédito, entender os fatores que levam ao cancelamento do serviço (*churn*) e simular o risco de evasão de novos perfis.
+    """)
 
-st.markdown("### 🧭 Navegação")
-st.markdown("""
-Utilize a barra lateral à esquerda para acessar as diferentes visões do projeto:
+    st.subheader("🧭 Navegação", anchor=False)
+    st.markdown("""
+    Utilize a barra lateral à esquerda para acessar as diferentes visões do projeto:
 
-*   **📈 01. Visão de Negócio:** Explore os perfis demográficos e a distribuição geral da base.
-*   **📊 02. Qualidade do Ativo:** Analise limites de crédito, volumetria de transações e comportamento financeiro.
-*   **⚙️ 03. Simulador de Churn:** Uma ferramenta interativa para testar perfis de clientes contra o modelo preditivo.
-""")
+    *   **📈 01. Visão de Negócio:** Explore os perfis demográficos e a distribuição geral da base.
+    *   **📊 02. Qualidade do Ativo:** Analise limites de crédito, volumetria de transações e comportamento financeiro.
+    *   **⚙️ 03. Simulador de Churn:** Uma ferramenta interativa para testar perfis de clientes contra o modelo preditivo.
+    """)
 
-st.info("👈 Abra o menu lateral clicando no ícone no canto superior esquerdo para começar.")
+    st.info("👈 Abra o menu lateral clicando no ícone no canto superior esquerdo para começar.")
+
+pagina_inicial = st.Page(
+    home_page,
+    title="Página Inicial", 
+    icon="🏠", 
+    default=True
+)
+
+visao_negocio = st.Page(
+    "pages/01_Visao_de_Negocio.py", 
+    title="Visão de Negócio", 
+    icon="📈"
+)
+
+qualidade_ativo = st.Page(
+    "pages/02_Qualidade_do_Ativo.py", 
+    title="Qualidade do Ativo", 
+    icon="📊"
+)
+
+simulador = st.Page(
+    "pages/03_Simulador_de_Churn.py", 
+    title="Simulador de Churn", 
+    icon="⚙️"
+)
+
+pg = st.navigation({
+    "Navegação": [pagina_inicial],
+    "Painéis": [visao_negocio, qualidade_ativo, simulador]
+})
+
+pg.run()
